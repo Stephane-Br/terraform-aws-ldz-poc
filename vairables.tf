e3dbad7c14c37b7387d9a80619ebe8efb3ec4859
sbrault <stf.bro@gmail.com>
2022-06-02 13:57:26 -0400

    _

---
+++ vairables.tf
@@ -0,0 +1,15 @@
+variable "region" {
+  type = string
+  default = "us-east-1"
+}
+
+
+variable "ami" {
+  type = string
+  default = "ami-0022f774911c1d690"
+}
+
+variable "instance_type" {
+  type = string
+  default = "t2.micro"
+}
